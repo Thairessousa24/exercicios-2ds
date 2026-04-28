2dabba5904208d9d83c60996863441fa09665da7
Thaires Sousa <thaires.rodrigues@aluno.ce.gov.br>
2026-04-28 13:55:05 +0000

    correção: adicionado variavel filmeFavorito

--- 2-bimestre/programacao-web/aula02-primeira-pagina-com-react-e-conhecendo-o-jsx/src/App.jsx
+++ 2-bimestre/programacao-web/aula02-primeira-pagina-com-react-e-conhecendo-o-jsx/src/App.jsx
@@ -1,9 +1,11 @@
 export default function App() {
   const materiaFavorita = 'Não Tenho'
+  const filmeFavorito = "Seu Filme"
   return (
     <div>
       <h1>Thaires Sousa</h1>
       <p>Minha matéria favorita é {materiaFavorita}</p>
+      <p>Meu filme favorito é: {filmeFavorito}</p>
     </div>
   )
 }
