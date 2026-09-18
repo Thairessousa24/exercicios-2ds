c920ffe3aeb944c687dc7c793813340bd02d4651
Thaires Sousa <thaires.rodrigues@aluno.ce.gov.br>
2026-09-18 19:41:09 +0000

    projeto-03

--- 3-bimestre/orientacao-a-objetos/projetos/projeto3/Factory.js
+++ 3-bimestre/orientacao-a-objetos/projetos/projeto3/Factory.js
@@ -1,9 +1,23 @@
 const Notificacao = require("./Notificacao")
 // CRIE SUA SOLUÇÃO ABAIXO ================
 
+class Factory {
+  static criar(tipo, mensagem) {
+    switch (tipo) {
+      case "email":
+        return new Notificacao.Email(mensagem)
 
+      case "sms":
+        return new Notificacao.SMS(mensagem)
 
+      case "app":
+        return new Notificacao.App(mensagem)
 
+      default:
+        throw new Error("Tipo de notificação inválido")
+    }
+  }
+}
 
 
 // === FIM DO CÓDIGO =======================
